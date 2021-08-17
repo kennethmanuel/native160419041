bf4102e127968cc99d63e955de976e09de2aca60
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-08-17 19:08:28 +0700

    Added toast

--- app/src/main/java/com/kennethmanuel/native160419041/MainActivity.kt
+++ app/src/main/java/com/kennethmanuel/native160419041/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kennethmanuel.native160419041
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "I am a Toast message", Toast.LENGTH_SHORT).show()
     }
 }
